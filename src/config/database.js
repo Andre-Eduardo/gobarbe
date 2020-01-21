3c1ee0b84e25e239b4f075a562f427401bd8833a
Andre Eduardo <andre.eduardo11@hotmail.com>
2020-01-20 22:26:53 -0300

    configurando mongo e mongoose

--- src/config/database.js
+++ src/config/database.js
@@ -11,3 +11,4 @@ module.exports = {
   },
 };
 // sudo docker run --name database -e POSTGRES_PASSWORD=docker -p 5432:5432 -d postgres
+// sudo docker run --name mongobarber -p 27017:27017 -d -t mongo
